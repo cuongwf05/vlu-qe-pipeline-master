ed9125ca3be3d5f0711af07c5ccdaf9919f5c484
Phan Ngọc Cường <cuong.2474802010057@vanlanguni.vn>
2026-09-17 17:22:54 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // Feature branch test - Quality Gate 1
 function login(username, password) {
-  return username === 'admin' && password === '123';
+  return username === 'admin' && password === '999';
 }
 
 module.exports = { login };
